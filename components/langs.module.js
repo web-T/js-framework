fc1a7241b093ce9567541a9375460e3897b8985d
goshi <chief@web-t.com.ua>
2015-07-09 08:47:36 +0300

    now languages module can translate in plural form

--- components/langs.module.js
+++ components/langs.module.js
@@ -29,5 +29,24 @@ webtFramework.inject({
 
         return what;
 
+    },
+
+    transPlural: function(what, value){
+
+        if (typeof value == 'undefined' || !value)
+            value = 0;
+
+        var mess = this.trans(what);
+
+        // getting days count
+        var d_cnt = Math.abs(value) % 100;
+        var n1 = d_cnt % 10;
+        if (d_cnt > 10 && d_cnt < 20) return typeof mess[5] != 'undefined' ? mess[5] : mess[1];
+        if (n1 > 1 && n1 < 5) return mess[2];
+        if (n1 == 1)
+            return mess[1];
+        else
+            return typeof mess[5] != 'undefined' ? mess[5] : mess[2];
+
     }
 });
